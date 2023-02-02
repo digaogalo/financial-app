1954eb898c5a08d1bb6fcdfdc87720fd506d9142
digaogalo <digaolimac@gmail.com>
2023-02-02 17:22:21 -0300

    code refactor

--- js/script.js
+++ js/script.js
@@ -6,12 +6,17 @@ const form = document.querySelector('#form')
 const inputTransactionName = document.querySelector('#text')
 const inputTransactionAmount = document.querySelector('#amount')
 
-const dummyTransactions = [
-  { id: 1, name: "Bolo de brigadeiro", amount: -20 },
-  { id: 2, name: "Salário", amount: 300 },
-  { id: 3, name: "Torta de Frango", amount: -10 },
-  { id: 4, name: "Violão", amount: 150 }
-];
+const localStorageTransactions = JSON.parse(localStorage  
+  .getItem('transactions'))
+let transactions = localStorage 
+.getItem('transactions') !== null ? localStorageTransactions : []
+
+const removeTransaction = ID => {
+  transactions = transactions.filter(transaction =>   
+    transaction.id !== ID)
+  updateLocalStorage()
+  init()
+}
 
 const addTransactionIntoDOM = (transaction) => {
     const operator = transaction.amount < 0 ? '-' : '+'
@@ -21,14 +26,17 @@ const addTransactionIntoDOM = (transaction) => {
 
     li.classList.add(CSSClass)
     li.innerHTML = `
-    ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span>
-    <button class="delete-btn">x</button>
+    ${transaction.name} 
+    <span>${operator} R$ ${amountWithoutOperator}</span>
+    <button class="delete-btn" onClick='removeTransaction(${transaction.id})'>  
+      x 
+    </button>
     `
   transactionsUl.append(li)
 };
 
 const updateBalanceValues = () => {
-    const transactionsAmounts = dummyTransactions   
+    const transactionsAmounts = transactions   
     .map(transaction => transaction.amount)
     const total = transactionsAmounts   
     .reduce((accumulator, transaction) => accumulator + transaction, 0) 
@@ -48,16 +56,51 @@ const updateBalanceValues = () => {
 }
 
 const init = () => {
-    dummyTransactions.forEach(addTransactionIntoDOM)
+    transactionsUl.innerHTML = ''
+    transactions.forEach(addTransactionIntoDOM)
     updateBalanceValues()
 }
 
-init()
+init() 
 
-form.addEventListener('submit', e => {
+const updateLocalStorage = () => {
+  localStorage.setItem('transactions', JSON.stringify(transactions))
+}
+
+const generateID = () => Math.round(Math.random() * 1000)
+
+const addToTransactionsArray = (transactionName, transactionAmount) => {
+  transactions.push({
+    id: generateID(),   
+    name: transactionName,  
+    amount: Number( transactionAmount )
+  })
+}
+
+const cleanInputs = () => {
+  inputTransactionName.value = ''
+  inputTransactionAmount.value = ''
+}
+
+const handleFormSubmit = e => {
   e.preventDefault()
 
-  if (inputTransactionName.value.trim() === '' || inputTransactionAmount.value.trim() === '') {
-    alert('Por favor, preencha o campo Nome e  valor da transação ')
+  const transactionName = inputTransactionName.value.trim()
+  const transactionAmount = inputTransactionAmount.value.trim()
+  const isSomeInputEmpty = transactionName === '' || transactionAmount === ''
+
+  if (isSomeInputEmpty) {
+    alert('Please fill in the Transaction name and amount field')
+    return
   }
-})
+  const transaction = { 
+    
+  }
+
+  addToTransactionsArray(transactionName, transactionAmount)
+  init()
+  updateLocalStorage()
+  cleanInputs()
+}
+
+form.addEventListener('submit', handleFormSubmit)
