216ae812efa4b1f37efaf7866c93fa6936b7e8b3
digaogalo <digaolimac@gmail.com>
2023-02-02 17:49:44 -0300

    destructuring

--- js/script.js
+++ js/script.js
@@ -18,37 +18,39 @@ const removeTransaction = ID => {
   init()
 }
 
-const addTransactionIntoDOM = (transaction) => {
-    const operator = transaction.amount < 0 ? '-' : '+'
-    const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
-    const amountWithoutOperator = Math.abs(transaction.amount)
+const addTransactionIntoDOM = ({ amount, name, id }) => {
+    const operator = amount < 0 ? '-' : '+'
+    const CSSClass = amount < 0 ? 'minus' : 'plus'
+    const amountWithoutOperator = Math.abs(amount)
     const li = document.createElement('li')
 
     li.classList.add(CSSClass)
     li.innerHTML = `
-    ${transaction.name} 
+    ${name} 
     <span>${operator} R$ ${amountWithoutOperator}</span>
-    <button class="delete-btn" onClick='removeTransaction(${transaction.id})'>  
-      x 
-    </button>
-    `
+    <button class="delete-btn" onClick='removeTransaction(${id})'>x </button>`
   transactionsUl.append(li)
 };
 
+const getExpenses = transactionsAmounts => Math.abs(transactionsAmounts 
+  .filter(value => value < 0) 
+  .reduce((accumulator, value) =>  accumulator + value, 0)) 
+  .toFixed(2)
+
+const getIncome = transactionsAmount => transactionsAmount
+    .filter((value) => value > 0)
+    .reduce((accumulator, value) => accumulator + value, 0)
+    .toFixed(2);
+
+const getTotal = transactionsAmounts => transactionsAmounts   
+.reduce((accumulator, transaction) => accumulator + transaction, 0) 
+.toFixed(2)
+
 const updateBalanceValues = () => {
-    const transactionsAmounts = transactions   
-    .map(transaction => transaction.amount)
-    const total = transactionsAmounts   
-    .reduce((accumulator, transaction) => accumulator + transaction, 0) 
-    .toFixed(2)
-    const income =transactionsAmounts   
-    .filter(value => value > 0) 
-    .reduce((accumulator, value) => accumulator + value, 0) 
-    .toFixed(2)
-    const expense = Math.abs(transactionsAmounts 
-    .filter(value => value < 0) 
-    .reduce((accumulator, value) =>  accumulator + value, 0)) 
-    .toFixed(2)
+    const transactionsAmounts = transactions.map(({ amount }) => amount)   
+    const total = getTotal(transactionsAmounts)
+    const income = getIncome(transactionsAmounts)
+    const expense = getExpenses(transactionsAmounts)
   
     balanceDisplay.textContent = `R$ ${total}`
     incomeDisplay.textContent = `R$ ${income}`
